9a13a54d04cd5e3a854943a6ca08977eab61d3a3
Dominik Rembis <dominik.rembis@gmail.com>
2020-04-03 17:41:35 +0200

    * Added 'letf panel', add github and home icon

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,49 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.leftPanel = new System.Windows.Forms.Panel();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnGithub = new System.Windows.Forms.Button();
+            this.leftPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(28)))), ((int)(((byte)(40)))));
+            this.leftPanel.Controls.Add(this.btnHome);
+            this.leftPanel.Controls.Add(this.btnGithub);
+            this.leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftPanel.Location = new System.Drawing.Point(0, 0);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(100, 561);
+            this.leftPanel.TabIndex = 0;
+            // 
+            // btnHome
+            // 
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnHome.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Image = global::CRM_Project.Properties.Resources.home;
+            this.btnHome.Location = new System.Drawing.Point(0, 120);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(100, 32);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // btnGithub
+            // 
+            this.btnGithub.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGithub.FlatAppearance.BorderSize = 0;
+            this.btnGithub.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnGithub.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnGithub.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGithub.Image = global::CRM_Project.Properties.Resources.github;
+            this.btnGithub.Location = new System.Drawing.Point(0, 20);
+            this.btnGithub.Name = "btnGithub";
+            this.btnGithub.Size = new System.Drawing.Size(100, 45);
+            this.btnGithub.TabIndex = 0;
+            this.btnGithub.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -37,16 +79,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(40)))), ((int)(((byte)(54)))));
             this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.leftPanel);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CRM Project";
+            this.leftPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel leftPanel;
+        private System.Windows.Forms.Button btnGithub;
+        private System.Windows.Forms.Button btnHome;
     }
 }
 
