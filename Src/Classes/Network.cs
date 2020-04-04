b2f3801c7b1e30254fad0b232d78987aacc6f977
Dominik Rembis <dominik.rembis@gmail.com>
2020-04-04 16:42:42 +0200

    * Added class 'newtwork' and method 'status'

---
+++ Src/Classes/Network.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_Project.Src.Classes
+{
+    class Network
+    {
+        public static bool status()
+        {
+            try
+            {
+                Ping ping = new Ping();
+                String host = "google.com";
+                byte[] buffer = new byte[32];
+                int timeout = 1000;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = ping.Send(host, timeout, buffer, pingOptions);
+                return (reply.Status == IPStatus.Success);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
