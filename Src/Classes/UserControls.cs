33310575f1f87ea13169651842802099d3145b73
Dominik Rembis <dominik.rembis@gmail.com>
2020-04-04 16:46:16 +0200

    * Added class 'UserControls' and method 'show'

---
+++ Src/Classes/UserControls.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRM_Project.Src.Classes
+{
+    class UserControls
+    {
+        public static void show(Control control, Control content)
+        {
+            content.Controls.Clear();
+
+            control.Dock = DockStyle.Fill;
+            control.BringToFront();
+            control.Focus();
+
+            content.Controls.Add(control);
+        }
+    }
+}
