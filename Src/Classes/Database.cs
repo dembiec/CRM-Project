2b91ae02dff1b4dc0c5d699544544a7b4637059f
Dominik Rembis <dominik.rembis@gmail.com>
2020-04-05 16:03:27 +0200

    * Added clas 'Database'

---
+++ Src/Classes/Database.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRM_Project.Src.Classes
+{
+    class Database
+    {
+        public static void manipulation(string queryContent)
+        {
+            string host = "localhost";
+            string dbUser = "root";
+            string dbPassword = "";
+            string dbName = "crm-project";
+
+            string connectionData = "datasource=" + host + ";port=3306;username=" + dbUser + ";password=" + dbPassword + ";database=" + dbName + ";";
+            string query = queryContent;
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionData);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+                MessageBox.Show("Poprawnie wykonano zapytanie do bazy danych");
+                databaseConnection.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Wystąpił błąd bazy danych");
+            }
+        }
+    }
+}
--- Src/Controls/UcCustomers.Designer.cs
+++ Src/Controls/UcCustomers.Designer.cs
@@ -28,25 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(23, 20);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(220, 60);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Dodaj nowego klienta";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // UcCustomers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(40)))), ((int)(((byte)(54)))));
-            this.Controls.Add(this.button1);
             this.Name = "UcCustomers";
             this.Size = new System.Drawing.Size(884, 501);
             this.ResumeLayout(false);
@@ -54,7 +42,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
