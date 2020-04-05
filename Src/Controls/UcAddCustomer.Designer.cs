bc64b2c28a2ba7d793ff44dea69879aa71e54f5d
Dominik Rembis <dominik.rembis@gmail.com>
2020-04-05 16:54:02 +0200

    * Added new control 'AcAddCustomer' (draft)

---
+++ Src/Controls/UcAddCustomer.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace CRM_Project.Src.Controls
+{
+    partial class UcAddCustomer
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UcAddCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(40)))), ((int)(((byte)(54)))));
+            this.Name = "UcAddCustomer";
+            this.Size = new System.Drawing.Size(884, 501);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
