01e607995b8d44f64cd3c3adbe4fcade45a6f18f
Dominik Rembis <dominik.rembis@gmail.com>
2020-04-08 16:59:53 +0200

    *Added customer support from the database and forwarding them to the table view

--- Src/Controls/UcCustomers.cs
+++ Src/Controls/UcCustomers.cs
@@ -26,18 +26,43 @@ namespace CRM_Project.Src.Controls
 
         private void UcCustomers_Load(object sender, EventArgs e)
         {
-            DataTable table = new DataTable();
-
-            table.Columns.Add("Id", typeof(int));
-            table.Columns.Add("Imię i Nazwisko", typeof(string));
-            table.Columns.Add("Adres E-mail", typeof(string));
-            table.Columns.Add("Numer Telefonu", typeof(int));
-
-            for (int i = 1; i <= 100; i++) {
-                table.Rows.Add(i, "Dominik Rembis", "dominik.rembis@vp.pl", 795167676);
+            Database.connect();
+            Database.query("SELECT * FROM `customers`");
+            if (Database.status) {
+                if (Database.reader.HasRows) {
+                    DataTable table = new DataTable();
+                    table.Columns.Add("Id", typeof(int));
+                    table.Columns.Add("Imię i Nazwisko", typeof(string));
+                    table.Columns.Add("Adres E-mail", typeof(string));
+                    table.Columns.Add("Numer Telefonu", typeof(string));
+                    table.Columns.Add("Adres Zamieszkania", typeof(string));
+                    table.Columns.Add("Dodatkowe Informacje", typeof(string));
+                    string description = "";
+                    while (Database.reader.Read()) {
+                        if (Database.reader.IsDBNull(5)) {
+                            description = "-";
+                        } else {
+                            description = Database.reader.GetString(5);
+                        }
+                        table.Rows.Add(Database.reader.GetString(0), Database.reader.GetString(1), Database.reader.GetString(2), Database.reader.GetString(3), Database.reader.GetString(4), description);
+                    }
+                    tblCustomers.DataSource = table;
+                } else {
+                    Label lblNoData = new Label();
+                    lblNoData.Name = "lblNoData";
+                    lblNoData.Top = 150;
+                    lblNoData.Left = 235;
+                    lblNoData.Height = 36;
+                    lblNoData.Width = 380;
+                    lblNoData.Text = "Brak Danych Do Wyświetlenia";
+                    lblNoData.Font = new Font("Calibri", 22);
+                    lblNoData.ForeColor = Color.FromArgb(243, 242, 251);
+                    tblCustomers.Controls.Add(lblNoData);
+                }
+            } else {
+                MessageBox.Show("Wystąpił błąd bazy danych podczas wykonywania zapytania");
             }
-
-            tblCustomers.DataSource = table;
+            Database.close();
         }
     }
 }
