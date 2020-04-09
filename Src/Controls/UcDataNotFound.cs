a96e911b3bcd4487f8cf29c6ec336b4c31d011fa
Dominik Rembis <dominik.rembis@gmail.com>
2020-04-09 15:13:06 +0200

    * Added new user contols 'UcDataNotFound'

---
+++ Src/Controls/UcDataNotFound.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRM_Project.Src.Controls
+{
+    public partial class UcDataNotFound : UserControl
+    {
+        public UcDataNotFound()
+        {
+            InitializeComponent();
+        }
+    }
+}
