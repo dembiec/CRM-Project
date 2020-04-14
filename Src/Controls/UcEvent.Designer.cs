88a699d325fb8c48c7122564240de7b7677ea3e2
Dominik Rembis <dominik.rembis@gmail.com>
2020-04-14 16:03:16 +0200

    * Added displaying events and options - search, refresh

--- Src/Controls/UcEvent.Designer.cs
+++ Src/Controls/UcEvent.Designer.cs
@@ -28,19 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.contentPanel = new System.Windows.Forms.Panel();
+            this.tblEvents = new System.Windows.Forms.DataGridView();
             this.btnAddEvent = new System.Windows.Forms.Button();
             this.btnRefresh = new System.Windows.Forms.Button();
             this.btnSearch = new System.Windows.Forms.Button();
             this.tbSearch = new System.Windows.Forms.TextBox();
+            this.contentPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tblEvents)).BeginInit();
             this.SuspendLayout();
             // 
             // contentPanel
             // 
+            this.contentPanel.Controls.Add(this.tblEvents);
             this.contentPanel.Location = new System.Drawing.Point(0, 50);
             this.contentPanel.Name = "contentPanel";
             this.contentPanel.Size = new System.Drawing.Size(884, 450);
             this.contentPanel.TabIndex = 0;
+            // 
+            // tblEvents
+            // 
+            this.tblEvents.AllowUserToAddRows = false;
+            this.tblEvents.AllowUserToDeleteRows = false;
+            this.tblEvents.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(46)))), ((int)(((byte)(60)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Calibri", 14.25F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(46)))), ((int)(((byte)(60)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            this.tblEvents.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.tblEvents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.tblEvents.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(38)))), ((int)(((byte)(51)))));
+            this.tblEvents.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tblEvents.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            this.tblEvents.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(46)))), ((int)(((byte)(60)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(91)))), ((int)(((byte)(112)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(46)))), ((int)(((byte)(60)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(91)))), ((int)(((byte)(112)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.tblEvents.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.tblEvents.ColumnHeadersHeight = 35;
+            this.tblEvents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(46)))), ((int)(((byte)(60)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Calibri", 12F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(46)))), ((int)(((byte)(60)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.tblEvents.DefaultCellStyle = dataGridViewCellStyle3;
+            this.tblEvents.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.tblEvents.EnableHeadersVisualStyles = false;
+            this.tblEvents.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(53)))), ((int)(((byte)(69)))));
+            this.tblEvents.Location = new System.Drawing.Point(20, 20);
+            this.tblEvents.Name = "tblEvents";
+            this.tblEvents.ReadOnly = true;
+            this.tblEvents.RowHeadersVisible = false;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(46)))), ((int)(((byte)(60)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Calibri", 14.25F);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(46)))), ((int)(((byte)(60)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            this.tblEvents.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.tblEvents.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.tblEvents.Size = new System.Drawing.Size(844, 410);
+            this.tblEvents.TabIndex = 0;
+            this.tblEvents.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tblEvents_CellDoubleClick);
             // 
             // btnAddEvent
             // 
@@ -74,6 +134,7 @@
             this.btnRefresh.TabIndex = 2;
             this.btnRefresh.Text = "Odśwież ";
             this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
             // btnSearch
             // 
@@ -90,6 +151,7 @@
             this.btnSearch.Size = new System.Drawing.Size(33, 33);
             this.btnSearch.TabIndex = 3;
             this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // tbSearch
             // 
@@ -113,6 +175,9 @@
             this.Controls.Add(this.btnSearch);
             this.Name = "UcEvent";
             this.Size = new System.Drawing.Size(884, 501);
+            this.Load += new System.EventHandler(this.UcEvent_Load);
+            this.contentPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tblEvents)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,5 +190,6 @@
         private System.Windows.Forms.Button btnRefresh;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.DataGridView tblEvents;
     }
 }
--- Src/Controls/UcEvent.cs
+++ Src/Controls/UcEvent.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CRM_Project.Src.Classes;
+using System.Text.RegularExpressions;
 
 namespace CRM_Project.Src.Controls
 {
@@ -23,6 +24,113 @@ namespace CRM_Project.Src.Controls
             UcAddEvent UcAE = new UcAddEvent();
             contentPanel.Dock = DockStyle.Fill;
             UserControls.show(UcAE, contentPanel);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            UcEvent_Load(sender, e);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (tbSearch.Text != "") {
+                string outData = Regex.Replace(tbSearch.Text, @"('|=|-{2})", string.Empty);
+                Database.connect();
+                Database.query("SELECT events.id, events.status, events.title, events.description, events.date, customers.name FROM `events` INNER JOIN `customers` ON events.customer_id = customers.id WHERE events.title LIKE '%"+outData+"%' OR customers.name LIKE '%"+outData+"%' ORDER BY events.date ASC");
+                if (Database.status) {
+                    if (Database.reader.HasRows) {
+                        DataTable table = new DataTable();
+                        table.Columns.Add("Id", typeof(int));
+                        table.Columns.Add("Status", typeof(string));
+                        table.Columns.Add("Tytuł", typeof(string));
+                        table.Columns.Add("Opis", typeof(string));
+                        table.Columns.Add("Data", typeof(string));
+                        table.Columns.Add("Imię i Nazwisko Klienta", typeof(string));
+                        string description = "";
+                        string status = "Aktywne";
+                        while (Database.reader.Read()) {
+                            if (Database.reader.IsDBNull(3)) {
+                                description = "-";
+                            } else {
+                                description = Database.reader.GetString(3);
+                            }
+
+                            if (Database.reader.GetBoolean(1)) {
+                                status = "Aktywne";
+                            } else {
+                                status = "Nieaktywne ";
+                            }
+                            table.Rows.Add(Database.reader.GetString(0), status, Database.reader.GetString(2), description, Database.reader.GetString(4), Database.reader.GetString(5));
+                        }
+                        tblEvents.DataSource = table;
+                    } else {
+                        MessageBox.Show("Dane Nie Zostały Znalezione");
+                    }
+                } else {
+                    contentPanel.Dock = DockStyle.Fill;
+                    UcDbError UcDE = new UcDbError();
+                    UserControls.show(UcDE, contentPanel);
+                }
+                Database.close();
+            }
+        }
+
+        private void UcEvent_Load(object sender, EventArgs e)
+        {
+            Database.connect();
+            Database.query("SELECT events.id, events.status, events.title, events.description, events.date, customers.name FROM `events` INNER JOIN `customers` ON events.customer_id = customers.id ORDER BY events.date ASC");
+            if (Database.status) {
+                if (Database.reader.HasRows) {
+                    DataTable table = new DataTable();
+                    table.Columns.Add("Id", typeof(int));
+                    table.Columns.Add("Status", typeof(string));
+                    table.Columns.Add("Tytuł", typeof(string));
+                    table.Columns.Add("Opis", typeof(string));
+                    table.Columns.Add("Data", typeof(string));
+                    table.Columns.Add("Imię i Nazwisko Klienta", typeof(string));
+                    string description = "";
+                    string status = "Aktywne";
+                    while (Database.reader.Read()) {
+                        if (Database.reader.IsDBNull(3)) {
+                            description = "-";
+                        } else {
+                            description = Database.reader.GetString(3);
+                        }
+
+                        if (Database.reader.GetBoolean(1)) {
+                            status = "Aktywne";
+                        } else {
+                            status = "Nieaktywne ";
+                        }
+                        table.Rows.Add(Database.reader.GetString(0), status, Database.reader.GetString(2), description, Database.reader.GetString(4), Database.reader.GetString(5));
+                    }
+                    tblEvents.DataSource = table;
+                } else {
+                    Label lblNoData = new Label();
+                    lblNoData.Name = "lblNoData";
+                    lblNoData.Top = 150;
+                    lblNoData.Left = 235;
+                    lblNoData.Height = 36;
+                    lblNoData.Width = 380;
+                    lblNoData.Text = "Brak Danych Do Wyświetlenia";
+                    lblNoData.Font = new Font("Calibri", 22);
+                    lblNoData.ForeColor = Color.FromArgb(243, 242, 251);
+                    tblEvents.Controls.Add(lblNoData);
+                }
+            } else {
+                contentPanel.Dock = DockStyle.Fill;
+                UcDbError UcDE = new UcDbError();
+                UserControls.show(UcDE, contentPanel);
+            }
+            Database.close();
+        }
+
+        private void tblEvents_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = int.Parse(tblEvents.Rows[e.RowIndex].Cells[0].Value.ToString());
+            UcUpdateEvent UcUE = new UcUpdateEvent(id);
+            contentPanel.Dock = DockStyle.Fill;
+            UserControls.show(UcUE, contentPanel);
         }
     }
 }
