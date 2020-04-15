af584766a1292637f15bde58a10bd6ce340e35db
Dominik Rembis <dominik.rembis@gmail.com>
2020-04-15 10:21:44 +0200

    * Added new user control 'UCUpdateEvent' and added alle updating functions

---
+++ Src/Controls/UcUpdateEvent.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace CRM_Project.Src.Controls
+{
+    partial class UcUpdateEvent
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.pbTitle = new System.Windows.Forms.PictureBox();
+            this.lblData = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.pbDate = new System.Windows.Forms.PictureBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.cbStatus = new System.Windows.Forms.ComboBox();
+            this.pbStatus = new System.Windows.Forms.PictureBox();
+            this.cbCustomer = new System.Windows.Forms.ComboBox();
+            this.pbCustomer = new System.Windows.Forms.PictureBox();
+            this.lblCustomer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTitle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(91)))), ((int)(((byte)(112)))));
+            this.lblTitle.Location = new System.Drawing.Point(20, 35);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(44, 19);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Tytuł";
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            this.tbTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbTitle.Font = new System.Drawing.Font("Calibri", 15.75F);
+            this.tbTitle.ForeColor = System.Drawing.Color.Black;
+            this.tbTitle.Location = new System.Drawing.Point(20, 60);
+            this.tbTitle.MaxLength = 100;
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(400, 33);
+            this.tbTitle.TabIndex = 1;
+            this.tbTitle.Validated += new System.EventHandler(this.tbTitle_Validated);
+            // 
+            // pbTitle
+            // 
+            this.pbTitle.Location = new System.Drawing.Point(400, 35);
+            this.pbTitle.Name = "pbTitle";
+            this.pbTitle.Size = new System.Drawing.Size(16, 16);
+            this.pbTitle.TabIndex = 2;
+            this.pbTitle.TabStop = false;
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.lblData.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(91)))), ((int)(((byte)(112)))));
+            this.lblData.Location = new System.Drawing.Point(464, 35);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(41, 19);
+            this.lblData.TabIndex = 3;
+            this.lblData.Text = "Data";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CalendarFont = new System.Drawing.Font("Calibri", 15.75F);
+            this.dateTimePicker.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            this.dateTimePicker.CustomFormat = "";
+            this.dateTimePicker.Font = new System.Drawing.Font("Calibri", 15.75F);
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(464, 60);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(400, 33);
+            this.dateTimePicker.TabIndex = 4;
+            this.dateTimePicker.Validated += new System.EventHandler(this.dateTimePicker_Validated);
+            // 
+            // pbDate
+            // 
+            this.pbDate.Location = new System.Drawing.Point(844, 35);
+            this.pbDate.Name = "pbDate";
+            this.pbDate.Size = new System.Drawing.Size(16, 16);
+            this.pbDate.TabIndex = 5;
+            this.pbDate.TabStop = false;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.lblStatus.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(91)))), ((int)(((byte)(112)))));
+            this.lblStatus.Location = new System.Drawing.Point(20, 125);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(52, 19);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Status";
+            // 
+            // cbStatus
+            // 
+            this.cbStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            this.cbStatus.Font = new System.Drawing.Font("Calibri", 15.75F);
+            this.cbStatus.ForeColor = System.Drawing.Color.Black;
+            this.cbStatus.FormattingEnabled = true;
+            this.cbStatus.Items.AddRange(new object[] {
+            "Aktywne",
+            "Nieaktywne"});
+            this.cbStatus.Location = new System.Drawing.Point(20, 150);
+            this.cbStatus.Name = "cbStatus";
+            this.cbStatus.Size = new System.Drawing.Size(400, 34);
+            this.cbStatus.TabIndex = 7;
+            this.cbStatus.Validated += new System.EventHandler(this.cbStatus_Validated);
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Location = new System.Drawing.Point(400, 125);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(16, 16);
+            this.pbStatus.TabIndex = 8;
+            this.pbStatus.TabStop = false;
+            // 
+            // cbCustomer
+            // 
+            this.cbCustomer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            this.cbCustomer.Font = new System.Drawing.Font("Calibri", 15.75F);
+            this.cbCustomer.ForeColor = System.Drawing.Color.Black;
+            this.cbCustomer.FormattingEnabled = true;
+            this.cbCustomer.Location = new System.Drawing.Point(464, 150);
+            this.cbCustomer.Name = "cbCustomer";
+            this.cbCustomer.Size = new System.Drawing.Size(400, 34);
+            this.cbCustomer.TabIndex = 9;
+            this.cbCustomer.MouseClick += new System.Windows.Forms.MouseEventHandler(this.cbCustomer_MouseClick);
+            this.cbCustomer.Validated += new System.EventHandler(this.cbCustomer_Validated);
+            // 
+            // pbCustomer
+            // 
+            this.pbCustomer.Location = new System.Drawing.Point(844, 125);
+            this.pbCustomer.Name = "pbCustomer";
+            this.pbCustomer.Size = new System.Drawing.Size(16, 16);
+            this.pbCustomer.TabIndex = 10;
+            this.pbCustomer.TabStop = false;
+            // 
+            // lblCustomer
+            // 
+            this.lblCustomer.AutoSize = true;
+            this.lblCustomer.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.lblCustomer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(91)))), ((int)(((byte)(112)))));
+            this.lblCustomer.Location = new System.Drawing.Point(464, 125);
+            this.lblCustomer.Name = "lblCustomer";
+            this.lblCustomer.Size = new System.Drawing.Size(49, 19);
+            this.lblCustomer.TabIndex = 11;
+            this.lblCustomer.Text = "Klient";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(91)))), ((int)(((byte)(112)))));
+            this.label1.Location = new System.Drawing.Point(20, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 19);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Dodatkowy Opis (opcjonalne)";
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            this.tbDescription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbDescription.Font = new System.Drawing.Font("Calibri", 15.75F);
+            this.tbDescription.ForeColor = System.Drawing.Color.Black;
+            this.tbDescription.Location = new System.Drawing.Point(20, 250);
+            this.tbDescription.MaxLength = 300;
+            this.tbDescription.Multiline = true;
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbDescription.Size = new System.Drawing.Size(844, 162);
+            this.tbDescription.TabIndex = 13;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(195)))), ((int)(((byte)(93)))));
+            this.btnUpdate.FlatAppearance.BorderSize = 0;
+            this.btnUpdate.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(195)))), ((int)(((byte)(93)))));
+            this.btnUpdate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(176)))), ((int)(((byte)(81)))));
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.btnUpdate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            this.btnUpdate.Location = new System.Drawing.Point(769, 440);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(95, 35);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Zaktualizuj";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(88)))), ((int)(((byte)(130)))));
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(88)))), ((int)(((byte)(130)))));
+            this.btnDelete.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(80)))), ((int)(((byte)(120)))));
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold);
+            this.btnDelete.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(242)))), ((int)(((byte)(251)))));
+            this.btnDelete.Location = new System.Drawing.Point(20, 440);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 35);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Usuń";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // UcUpdateEvent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(38)))), ((int)(((byte)(51)))));
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.tbDescription);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCustomer);
+            this.Controls.Add(this.pbCustomer);
+            this.Controls.Add(this.cbCustomer);
+            this.Controls.Add(this.pbStatus);
+            this.Controls.Add(this.cbStatus);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pbDate);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.pbTitle);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "UcUpdateEvent";
+            this.Size = new System.Drawing.Size(884, 501);
+            this.Load += new System.EventHandler(this.UcUpdateEvent_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTitle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.PictureBox pbTitle;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.PictureBox pbDate;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ComboBox cbStatus;
+        private System.Windows.Forms.PictureBox pbStatus;
+        private System.Windows.Forms.ComboBox cbCustomer;
+        private System.Windows.Forms.PictureBox pbCustomer;
+        private System.Windows.Forms.Label lblCustomer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ Src/Controls/UcUpdateEvent.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CRM_Project.Src.Classes;
+using System.Text.RegularExpressions;
+
+namespace CRM_Project.Src.Controls
+{
+    public partial class UcUpdateEvent : UserControl
+    {
+        private int idEvent;
+        public UcUpdateEvent(int id)
+        {
+            idEvent = id;
+            InitializeComponent();
+        }
+
+        private int idCustomer;
+        private void UcUpdateEvent_Load(object sender, EventArgs e)
+        {
+            Database.connect();
+            Database.query("SELECT * FROM `events` WHERE id = "+idEvent);
+            if (Database.status) {
+                if (Database.reader.HasRows) {
+                    while (Database.reader.Read()) {
+                        idCustomer = int.Parse(Database.reader.GetString(1));
+                        tbTitle.Text = Database.reader.GetString(2);
+                        dateTimePicker.CustomFormat = "dd-MM-yyy HH:mm";
+                        dateTimePicker.Text = Database.reader.GetString(5);
+                        if (Database.reader.GetBoolean(4)) {
+                            cbStatus.SelectedIndex = 0;
+                        } else {
+                            cbStatus.SelectedIndex = 1;
+                        }
+
+                        if (Database.reader.IsDBNull(3)) {
+                            tbDescription.Text = string.Empty;
+                        } else {
+                            tbDescription.Text = Database.reader.GetString(3);
+                        }
+                    }
+                } else {
+                    MessageBox.Show("Dane Nie Zostały Znalezione");
+                }
+            } else {
+                MessageBox.Show("Błąd Bazy Danych");
+            }
+            Database.close();
+
+            Database.connect();
+            Database.query("SELECT id, name, email FROM `customers` ORDER BY name ASC");
+            if (Database.status) {
+                if (Database.reader.HasRows) {
+                    List<Customers> list = new List<Customers>();
+                    while (Database.reader.Read()) {
+                        list.Add(new Customers() { ID = int.Parse(Database.reader.GetString(0)), Name = Database.reader.GetString(1) + " - " + Database.reader.GetString(2) });
+                    }
+                    cbCustomer.DataSource = list;
+                    cbCustomer.ValueMember = "ID";
+                    cbCustomer.DisplayMember = "Name";
+                    cbCustomer.SelectedValue = idCustomer;
+                }
+            } else {
+                MessageBox.Show("Bład bazy danych");
+            }
+            Database.close();
+        }
+
+        //title
+        bool title = true;
+        private void tbTitle_Validated(object sender, EventArgs e)
+        {
+            if (tbTitle.Text == "") {
+                title = false;
+                pbTitle.Image = Properties.Resources.not_accept;
+            } else {
+                title = true;
+                pbTitle.Image = Properties.Resources.check;
+            }
+        }
+
+        //date
+        bool date = true;
+        private void dateTimePicker_Validated(object sender, EventArgs e)
+        {
+            if (dateTimePicker.Text == " ") {
+                date = false;
+                pbDate.Image = Properties.Resources.not_accept;
+            } else {
+                date = true;
+                pbDate.Image = Properties.Resources.check;
+            }
+        }
+
+        //status
+        bool status = true;
+        private void cbStatus_Validated(object sender, EventArgs e)
+        {
+            if (dateTimePicker.Text == " ") {
+                status = false;
+                pbStatus.Image = Properties.Resources.not_accept;
+            } else {
+                status = true;
+                pbStatus.Image = Properties.Resources.check;
+            }
+        }
+
+        //customer
+        private void cbCustomer_MouseClick(object sender, MouseEventArgs e)
+        {
+            cbCustomer.DroppedDown = true;
+        }
+
+        bool customer = true;
+        private void cbCustomer_Validated(object sender, EventArgs e)
+        {
+            if (cbCustomer.Text == "") {
+                customer = false;
+                pbCustomer.Image = Properties.Resources.not_accept;
+            } else {
+                customer = true;
+                pbCustomer.Image = Properties.Resources.check;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Database.connect();
+            Database.query("DELETE FROM `events` WHERE `events`.`id` = "+idEvent);
+            if (Database.status) {
+                btnDelete.Enabled = false;
+                btnUpdate.Enabled = false;
+
+                tbTitle.Clear();
+                pbTitle.Image = null;
+
+                dateTimePicker.Format = DateTimePickerFormat.Custom;
+                dateTimePicker.CustomFormat = " ";
+                pbDate.Image = null;
+
+                cbStatus.ResetText();
+                pbStatus.Image = null;
+
+                cbCustomer.ResetText();
+                pbCustomer.Image = null;
+
+                tbDescription.Text = "";
+
+                MessageBox.Show("Poprawnie usunięto wydarzenie z bazy danych");
+            } else {
+                MessageBox.Show("Wystąpił błąd bazy danych. Spróbuj ponownie za chwilę");
+            }
+            Database.close();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (title && date && status && customer) {
+                string dbQuery;
+                Customers Selectedcustomer = cbCustomer.SelectedItem as Customers;
+                string outTitle= Regex.Replace(tbTitle.Text, @"('|=|-{2})", string.Empty);
+                string outDescription;
+                int selectedStatus = 1;
+                string dateFormat = Convert.ToDateTime(dateTimePicker.Text).ToString("yyyy-MM-dd HH:mm:ss");
+                if (tbDescription.Text == "") {
+                    outDescription = string.Empty;
+                } else {
+                    outDescription = Regex.Replace(tbDescription.Text, @"('|=|-{2})", string.Empty);
+                }
+
+                if (cbStatus.SelectedIndex == 0) {
+                    selectedStatus = 1;
+                } else {
+                    selectedStatus = 0;
+                }
+                dbQuery = "UPDATE `events` SET `customer_id` = '"+ Selectedcustomer.ID+ "', `title` = '"+outTitle+"', `description` = '"+outDescription+"', `status` = '"+selectedStatus+"', `date` = '"+dateFormat+"', `update_date` = current_timestamp() WHERE `events`.`id` = " + idEvent;
+
+                Database.connect();
+                Database.query(dbQuery);
+                if (Database.status) {
+                    UcUpdateEvent_Load(sender, e);
+                    MessageBox.Show("Poprawnie zaktualizowano dane klienta w bazie danych");
+                } else {
+                    MessageBox.Show("Wystąpił błąd bazy danych. Spróbuj ponownie za chwilę");
+                }
+                Database.close();
+            } else {
+                MessageBox.Show("Sprawdź Wszystkie Pola");
+            }
+        }
+    }
+}
